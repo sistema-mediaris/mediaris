60216f53c76066a2a608bcbb559ff824b8f4fef1
Cristiano Fromagio <fromagio_cristiano@live.com>
2016-11-10 02:08:27 -0200

    Atualizando models usuário e user para manipulação de dados na autenticação/banco

--- app/User.php
+++ app/User.php
@@ -10,13 +10,18 @@ class User extends Authenticatable
     use Notifiable;
 
     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'usuarios';
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $fillable = ['name', 'email', 'avatar', 'super', 'provider', 'social_id'];
 
     /**
      * The attributes that should be hidden for arrays.
